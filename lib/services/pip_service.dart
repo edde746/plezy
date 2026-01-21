88bb3b2222531cf39edbe9cccbb32d36a32db0e0
Sven McCall Jr. <andrew.sven.mccall@gmail.com>
2026-01-21 11:01:05 -0500

    feat(pip): add macOS support with playback control callbacks
    Extends PipService to support macOS alongside Android, adding callbacks for play/pause and seek commands from native PiP overlay controls, plus method to update play/pause icon state.

--- lib/services/pip_service.dart
+++ lib/services/pip_service.dart
@@ -9,18 +9,31 @@ class PipService {
   factory PipService() => _instance;
 
   PipService._internal() {
-    // Listen for callbacks from native Android
+    // Listen for callbacks from native platforms (macOS and Android)
     _channel.setMethodCallHandler(_handleMethodCall);
   }
 
   /// ValueNotifier for PiP state - widgets can listen to this
   final ValueNotifier<bool> isPipActive = ValueNotifier<bool>(false);
 
+  /// Callback for play/pause commands from PiP overlay
+  void Function()? onPlayPause;
+
+  /// Callback for seek commands from PiP overlay (offset in seconds)
+  void Function(int offset)? onSeek;
+
   Future<dynamic> _handleMethodCall(MethodCall call) async {
     switch (call.method) {
       case 'onPipChanged':
         final isInPip = call.arguments as bool;
         isPipActive.value = isInPip;
+        break;
+      case 'playPause':
+        onPlayPause?.call();
+        break;
+      case 'seek':
+        final offset = call.arguments as int;
+        onSeek?.call(offset);
         break;
     }
   }
@@ -29,7 +42,15 @@ class PipService {
     return await _channel.invokeMethod<bool>('isSupported') ?? false;
   }
 
-  static Future<void> enter({int? width, int? height}) async {
-    await _channel.invokeMethod('enter', {'width': width, 'height': height});
+  static Future<void> enter({int? width, int? height, bool? isPlaying}) async {
+    await _channel.invokeMethod('enter', {
+      'width': width,
+      'height': height,
+      'isPlaying': isPlaying,
+    });
+  }
+
+  static Future<void> updatePlayPauseIcon({required bool isPlaying}) async {
+    await _channel.invokeMethod('updatePlayPauseIcon', isPlaying);
   }
 }
